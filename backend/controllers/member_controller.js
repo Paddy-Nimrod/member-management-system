1a65cb32e32a1a01077bafded0f2fb96abaa869e
Paddy-Nimrod <n.ambetsa21@gmail.com>
2024-12-07 17:09:02 +0300

    created member controllers

---
+++ backend/controllers/member_controller.js
@@ -0,0 +1,9 @@
+exports.createNewMember = (req, res) => { }
+
+exports.getAllMembers = (req, res) => { }
+
+exports.getMemberById = (req, res) => { }
+
+exports.updateMemberDetails = (req, res) => { }
+
+exports.deleteMember = (req, res) => {}

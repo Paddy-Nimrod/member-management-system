f481febfb633321d287a071bb6242d1f869c26c9
Paddy-Nimrod <n.ambetsa21@gmail.com>
2024-12-08 17:18:06 +0300

    created member model

---
+++ backend/migrations/20241208141527-create-member.js
@@ -0,0 +1,58 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Members', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      dateOfBirth: {
+        type: Sequelize.DATE
+      },
+      membershipStartDate: {
+        type: Sequelize.DATE
+      },
+      MembershipEndDate: {
+        type: Sequelize.DATE
+      },
+      membershipStatus: {
+        type: Sequelize.ENUM("Active","Inactive","Suspended")
+      },
+      profilePicture: {
+        type: Sequelize.STRING
+      },
+      notes: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Members');
+  }
+};

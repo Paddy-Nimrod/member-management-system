8f163500971d9d911ab2691714a31f0421e2741e
Paddy-Nimrod <n.ambetsa21@gmail.com>
2024-12-12 05:41:10 +0300

    created pages files

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-slate-800 h-4 w-full flex">
+      <div className="flex">
+        <ul>
+          <li className="text-white">Member system</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
